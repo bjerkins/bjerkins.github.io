693ca65ed0a4bee894144371c5de4f2d907c6f0d
Bjarki Madsen <bjerkins8@gmail.com>
2015-10-25 00:46:14 +0200

    adding cv page

---
+++ scripts/app.js
@@ -0,0 +1,1 @@
+!function(e){"use strict";var n=e.querySelector("#app");n.addEventListener("dom-change",function(){console.log("Our app is ready to rock!")}),window.addEventListener("WebComponentsReady",function(){})}(document);
